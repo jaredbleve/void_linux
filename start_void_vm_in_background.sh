d098c7b3bb159e9de9a8772ebd0345e2034e086a
Jared Leve <jaredbleve@gmail.com>
2025-03-28 13:15:13 -0700

    fixed backgrounding void vm script

--- start_void_vm_in_background.sh
+++ start_void_vm_in_background.sh
@@ -1,10 +1,1 @@
-nohup qemu-system-x86_64 \
-  -m 4096 \
-  -smp 4 \
-  -vga std \
-  -drive file=voidlinux.qcow2,format=qcow2 \
-  -boot c \
-  > void_vm.log 2>&1 &
-
-disown
-
+nohup qemu-system-x86_64 -m 4096 -smp 4 -vga std -drive file=voidlinux.qcow2,format=qcow2 -boot c > void_vm.log 2>&1 &
